6fe749fb2c7ef197363168c14c79df7e47e17843
quadrismegistus <ryan.heuser@gmail.com>
2021-03-29 16:22:03 +0100

    updates

--- cadence/__init__.py
+++ cadence/__init__.py
@@ -1,1 +1,10 @@
+"""cadence - Minimalist rhythm processor"""
+
+__version__ = '0.1.0'
+__author__ = 'Ryan Heuser <rj416@am.ac.uk>'
+__all__ = []
+
+
+
 from .imports import *
+from .cadence import *
